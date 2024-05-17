cf107b0c6dea4f48c8d48a88429a4e710e07b82a
ddas <debainfo24@gmail.com>
2024-05-17 18:35:30 +0530

    code added for some sorting

---
+++ src/main/java/org/icims/dsa/logical/InsertionSort.java
@@ -0,0 +1,34 @@
+package org.icims.dsa.logical;
+
+public class InsertionSort {
+
+	public static void main(String[] args) {
+
+		int arr[]= {7,8,3,1,2,0,5,-1};
+		System.out.print("Before sort the array is: ");
+		for(int i=0;i<arr.length;i++) {
+			System.out.print(arr[i]+" ");
+		}
+		System.out.println();
+		//bubble sort O(n^2)
+		for(int i=1;i<arr.length;i++) {
+			int current=arr[i];
+			int j=i-1;
+			while(j>=0 && current <arr[j]) {
+				arr[j+1]= arr[j];
+				j--;
+			}
+			// placement
+			arr[j+1]=current;
+		}
+		printArray(arr);
+	}
+
+	private static void printArray(int[] arr) {
+		System.out.print("After sort the array using insertion sort is: ");
+		for(int i=0;i<arr.length;i++) {
+			System.out.print(arr[i]+" ");
+		}
+	}
+
+}
